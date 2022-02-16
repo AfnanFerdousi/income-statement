45b225f2edff25e61c8e64a91be933cfdcf135f7
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-02-17 00:19:15 +0600

    save button events updated

--- js/script.js
+++ js/script.js
@@ -1,42 +1,69 @@
 // Income
 const income = document.getElementById("income");
-
 // Expenses
 const food = document.getElementById("food");
 const rent = document.getElementById("rent");
 const cloth = document.getElementById("cloth");
-
+// calculate button
+let calculate = document.getElementById("calculate");
 let totalExpense = document.getElementById("total-expense");
 let balance = document.getElementById("balance");
+// Save button
+let save = document.getElementById("save");
+const rate = document.getElementById("rate");
+const saveAmount = document.getElementById("save-amount");
+const remainingBalance = document.getElementById("remaining-balance");
 
+// Clear input function
+function clearInput() {
+    food.value = "";
+    rent.value = "";
+    cloth.value = "";
+    income.value = "";
+    rate.value = "";
+}
 
 // Calulate button function
-document.getElementById("calculate").addEventListener('click', function () {
-    
+calculate.addEventListener('click', function () {    
+    const addExpenses = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(cloth.value);
+    const restBalance = parseFloat(income.value) - parseFloat(addExpenses);    
     // Negative Number Error Handling
     if (food.value < 0 || rent.value < 0 || cloth.value < 0 || income.value < 0) {
         alert("Please enter a valid number!")        
     }
-
     else {
         // Expense greater than income error handling
-        if (totalExpense > income) {
+        if (addExpenses > income.value) {
             alert("Please Re-check you income");
-            food.value = "";
-            rent.value = "";
-            cloth.value = "";
-            income.value = "";
+            // food.value = "";
+            // rent.value = "";
+            // cloth.value = "";
+            // income.value = "";
+            clearInput();
         }     
         else {
-            const addExpenses = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(cloth.value);
             totalExpense.innerText = addExpenses;
-    
-            const restBalance = parseFloat(income.value) - parseFloat(addExpenses);
             balance.innerText = restBalance;            
-        }
-    
+        }    
     }    
 })
+// Save button function
+save.addEventListener('click', function () {  
+    const percentage = parseFloat(rate.value) / 100;   
+    const saveTotal = parseFloat(income.value) * parseFloat(percentage);  
+
+    if (balance.innerText < saveTotal) {
+        alert("you have exceded your limit");
+        clearInput();
+    }   
+    else {
+    saveAmount.innerText = saveTotal;   
+    const remain = parseFloat(balance.innerText) - saveAmount.innerText;
+    remainingBalance.innerText = remain; 
+    }
+})
+
+
 
 
 
